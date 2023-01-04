6a4ee6142ad0cdbc88d35b3a65111df6bd1eee8c
gnawinganimal <unrongeur@proton.me>
2023-01-04 00:56:37 -0800

    Allow user to specify memory

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,22 @@
 use clap::Parser;
 use std::fs;
 
+use crate::runtime::Runtime;
+
 pub mod runtime;
 
 #[derive(Parser, Debug)]
 #[command(author = "Evelyn Heller", version, about = "Brainfuck interpreter", long_about = None)]
 struct Cli {
-    path: String,
+    path: String, // path to the brainfuck source
+
+    #[arg(short = 'm', long = "mem", default_value_t = 30000)]
+    mem_size: usize, // amount of memory to allocate
 }
 
 fn main() {
-    let Cli { path } = Cli::parse();
+    let Cli { path, mem_size } = Cli::parse();
 
-    let src = fs::read_to_string(path).expect("Could not read file");
-
-    println!("{}", src);
+    let _ = fs::read_to_string(path).expect("Could not read file");
+    let _ = Runtime::new(mem_size);
 }
