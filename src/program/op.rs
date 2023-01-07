5736b9fec5b6878969c74e014ea2cb4b43e6ec99
gnawinganimal <unrongeur@proton.me>
2023-01-07 00:01:33 -0800

    Add display to operations

--- src/program/op.rs
+++ src/program/op.rs
@@ -1,3 +1,6 @@
+use std::fmt::Display;
+
+pub use Operation::*;
 
 #[derive(Clone, Copy, Eq, PartialEq, Debug)]
 pub enum Operation {
@@ -9,4 +12,21 @@ pub enum Operation {
     Write,
     Jump(usize),
     Back(usize),
+}
+
+impl Display for Operation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            AddPtr(n) => ">".repeat(*n as usize),
+            SubPtr(n) => "<".repeat(*n as usize),
+            AddCur(n) => "+".repeat(*n as usize),
+            SubCur(n) => "-".repeat(*n as usize),
+            Read => ",".to_string(),
+            Write => ".".to_string(),
+            Jump(_) => "[".to_string(),
+            Back(_) => "]".to_string(),
+        };
+
+        write!(f, "{}", s)
+    }
 }
